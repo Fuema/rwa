5f4f000703d2b853263b4c1ec764102ffaf0e9b1
Fuema <47659717+Fuema@users.noreply.github.com>
2021-02-08 16:55:43 -0500

    Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+// HttpClient module for RESTful API
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { QuestionComponent } from './components/question/question.component';
+import { TagComponent } from './components/tag/tag.component';
+import { CategoryComponent } from './components/category/category.component';
+import { CategoryService, TagService, QuestionService } from './services';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionComponent,
+    TagComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    CategoryService, TagService, QuestionService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
